89a44fc63c52c65f04ac9fa6f39ca89f96056945
Marc S. Brooks <marc@mbrooks.info>
2026-05-17 16:15:51 -0700

    Added a rudimentary HTTP :data encoder

---
+++ lib/encoder.go
@@ -0,0 +1,37 @@
+//
+//  go-webview-src-builder
+//  Create a self-contained binary of HTML sources in a WebView
+//
+//  Copyright 2026, Marc S. Brooks (https://mbrooks.info)
+//  Licensed under the MIT license:
+//  http://www.opensource.org/licenses/mit-license.php
+//
+
+package lib
+
+import (
+	"encoding/base64"
+	"mime"
+	"net/http"
+)
+
+// Generate browser HTTP Data URL (HTTP data:)
+func EncodeData(v []byte, fileExt string) string {
+	var mimeType string = getMimeType(fileExt)
+
+	if mimeType == "" {
+		mimeType = http.DetectContentType(v)
+	}
+
+	return "data:" + mimeType + ";base64," + toBase64(v)
+}
+
+// Get MIME type for given file type.
+func getMimeType(v string) string {
+	return mime.TypeByExtension(v)
+}
+
+// Convert bytes to Base64 string value.
+func toBase64(v []byte) string {
+	return base64.StdEncoding.EncodeToString(v)
+}
